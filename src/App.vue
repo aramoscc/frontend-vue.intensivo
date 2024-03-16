cb41c9ce39b1553cb04407aa405b0500bb2e91e9
Alejandro <alejandroramosc2002@gmail.com>
2024-03-16 13:39:42 +0100

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<script setup>
+  import{ref,onMounted} from "vue"
+  import Formulario from "./Formulario.vue"
+  import Item from "./Item.vue"
+
+  let colores = ref([])
+
+  onMounted(() => {
+    fetch("https://backend-colores-intensivo-9t6u.onrender.com/colores")
+    .then(respuesta => respuesta.json())
+    .then(respuestaProcesada => colores.value = respuestaProcesada)   
+  })
+
+  function crearColor(color){
+    fetch("https://backend-colores-intensivo-9t6u.onrender.com/colores/nuevo", {
+      method: "POST",
+      body: JSON.stringify(color),
+      headers: {
+        "Content-type" : "application/json"
+      }
+    })
+    .then(respuesta => respuesta.json())
+    .then(({id,error}) => {
+      if(!error){
+        return colores.value.push({id,...color});
+      }
+      console.log("error al usuario");
+    })
+  }
+
+  function borrarColor(id){
+    fetch(`https://backend-colores-intensivo-9t6u.onrender.com/colores/borrar/${id}`,{
+      method: "DELETE"
+    })
+    .then(respuesta => respuesta.json())
+    .then(({error}) => {
+      if(!error){
+        return colores.value = colores.value.filter(color => color.id != id);
+      }
+      console.log("error al usuario");
+    })
+  }
+  /*
+  export default {
+    data(){
+      return {
+        colores : []
+      }
+    },
+    methods : {
+      crearColor(color){
+        fetch("http://localhost:4000/colores/nuevo", {
+          method: "POST",
+          body: JSON.stringify(color),
+          headers: {
+            "Content-type" : "application/json"
+          }
+        })
+        .then(respuesta => respuesta.json())
+        .then(({id,error}) => {
+          if(!error){
+            return colores.value.push({id,...color});
+          }
+          console.log("error al usuario");
+        })
+      },
+      borrarColor(id){
+        fetch(`http://localhost:4000/colores/borrar/${id}`,{
+          method: "DELETE"
+        })
+        .then(respuesta => respuesta.json())
+        .then(({error}) => {
+          if(!error){
+            return colores.value = colores.value.filter(color => color.id != id);
+          }
+          console.log("error al usuario");
+        })
+      }
+    },
+    components : {
+      Formulario,
+      Item
+    },
+    mounted(){
+      fetch("http://localhost:4000/colores")
+      .then(respuesta => respuesta.json())
+      .then(respuestaProcesada => colores.value = respuestaProcesada)
+    }
+  }
+  */
+</script>
+
+<template>
+  <Formulario @crear="crearColor" />
+  <ul>
+    <Item v-for="({id,r,g,b}) in colores" :id="id" :r="r" :g="g" :b="b" @borrar="borrarColor"/>
+  </ul>
+</template>
---
+++ src/Item.vue
@@ -0,0 +1,10 @@
+<script setup >
+    defineProps(["id","r","g","b"])
+    const emit = defineEmits(["borrar"])
+</script>
+
+<template>
+    <li :style="{backgroundColor : `rgb(${[r,g,b].join(',')})`}"
+    @click="emit('borrar', id)"
+    >{{ r }},{{ g }},{{ b }}</li>
+</template>
